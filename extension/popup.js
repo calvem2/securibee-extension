98acc9803698b29127bed14f07a7071b72efe879
Linda Vong <lwvong@cs.washington.edu>
2021-02-01 18:30:42 -0800

    display info-tracking text relevant to page being viewed in extension popup (but it's appending instead of replacing text)

--- extension/popup.js
+++ extension/popup.js
@@ -20,32 +20,42 @@ window.onload = function() {
             }
         }
     }
-
-    // log raw url and domain name -- upon window load (adapted below)
-    // chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-    //     let url = tabs[0].url;
-    //     // use `url` here inside the callback because it's asynchronous!
-    //     console.log('raw url: ', url);
-        
-    //     var domain = url.replace('http://','').replace('https://','').replace('www','').replace('.com','').replace('.edu','').replace('.org','').split(/[/?#]/)[0];
-    //     // .replace('','')
-
-    //     console.log('domain: ', domain);
-    // });
 };
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    // log raw url and domain name -- upon tabs update (adapted from above)
-    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-        let url = tab.url;
-        // use `url` here inside the callback because it's asynchronous!
-        console.log('raw url: ', url);
-        
-        var domain = url.replace('http://','').replace('https://','').replace('www','').replace('.com','').replace('.edu','').replace('.org','').split(/[/?#]/)[0].replace('.','');
-        // .replace('','')
+    if (changeInfo.status === 'complete') {
+        chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+            // extract raw url and domain name of current tab
+            let url = tab.url;
+            console.log('raw url: ', url);
+            var domain = url.replace('http://','').replace('https://','').replace('www.','').replace('.com','').replace('.edu','').replace('.org','').split(/[/?#]/)[0].replace('.','');
+            console.log('domain: ', domain);
 
-        console.log('domain: ', domain);
-    });
+            // parse info-tracker.json into a obj
+            var request = new XMLHttpRequest();
+            request.open("GET", "./info-tracker.json", false);
+            request.send(null)
+            var obj = JSON.parse(request.responseText);
+            // console.log('object: ', obj);
+            // console.log('part??: ', obj.platforms.p[0].name);
+            
+            // look through all platform names included in json
+            for (var i = 0; i < obj.platforms.p.length; i++) {
+                // if the domain name matches a platform included in the json,
+                // update the text displayed on extension info-tracker tab
+                if (obj.platforms.p[i].name == domain) {
+                    var textOne = printValues(obj.platforms.p[i].what);
+                    document.getElementById("one").innerHTML = textOne;
+
+                    var textTwo = printValues(obj.platforms.p[i].who);
+                    document.getElementById("two").innerHTML = textTwo;
+
+                    var textThree = printValues(obj.platforms.p[i].so);
+                    document.getElementById("three").innerHTML = textThree;       
+                }
+            }
+        });
+    }
 
     /* TODO: add rules for which extension page to show based on tab url ?
      * (or potentially change to page action)
@@ -77,3 +87,16 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     // }
 });
 
+// Define recursive function to print nested values
+function printValues(obj) {
+    for(var k in obj) {
+        if(obj[k] instanceof Object) {
+            return printValues(obj[k]);
+        } else {
+            // document.write(obj[k] + "<br>");
+            var tblRow = "<p>" + obj[k] + "</p>";
+
+            return document.getElementById("one").innerHTML + tblRow;
+        };
+    }
+};
