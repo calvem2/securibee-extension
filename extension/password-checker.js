6cdc42d0f363f9d540fcc1ef4bd1da0301555cd7
clairebeard <beard.claire@hotmail.com>
2021-02-05 17:37:48 -0800

    Case, symbol, and repeating character check for password checker.

--- extension/password-checker.js
+++ extension/password-checker.js
@@ -17,19 +17,34 @@ window.onload = function() {
     }
 
     function validatePassword() {
+        if (!passwordEmpty(passwordChecker.value)) {
+            checkLength(passwordChecker.value);
+            commonSequence(passwordChecker.value);
+            containsNumber(passwordChecker.value);
+            containsSymbol(passwordChecker.value);
+            noSameLetterSequences(passwordChecker.value);
+            checkCase(passwordChecker.value);
+        }
+    }
+
+    // Check the length of the password
+    function checkLength(password) {
         // Check length
-        if (passwordChecker.value.length < PASSWORD_LENGTH) {
+        if (password.length < PASSWORD_LENGTH) {
             document.getElementById("password-length").style.color = "red";
         } else {
             document.getElementById("password-length").style.color = "green";
         }
+    }
 
+    // Check to see if any common sequences are in the password
+    function commonSequence(password) {
         // Check common sequences
         document.getElementById("password-common").style.color = "green";
         let matchedSeqs = "";   // common sequences detected
         for (let i = 0; i < COMMON_SEQUENCES.length; i++) {
             // If common sequence detected, change color and append sequence to matchedSeqs for user feedback
-            if (passwordChecker.value.includes(COMMON_SEQUENCES[i])) {
+            if (password.includes(COMMON_SEQUENCES[i])) {
                 document.getElementById("password-common").style.color = "red";
                 matchedSeqs = matchedSeqs.length === 0 ? "(" + COMMON_SEQUENCES[i] : matchedSeqs + ", " + COMMON_SEQUENCES[i];
             }
@@ -38,6 +53,54 @@ window.onload = function() {
             matchedSeqs += ")";
         }
         document.querySelector("#password-common span").innerHTML = matchedSeqs;
+    }
 
+    // Check that there is one upper and lowercase letter in the password
+    function checkCase(password) {
+        if (new RegExp('[a-z]+').test(password) && new RegExp('[A-Z]+').test(password)) {
+            document.getElementById("password-case").style.color = "green";
+        } else {
+            document.getElementById("password-case").style.color = "red";
+        }
+    }
+
+    // Contains at least one number in the password
+    function containsNumber(password) {
+        if (new RegExp('[0-9]+').test(password)) {
+            document.getElementById("password-numbers").style.color = "green";
+        } else {
+            document.getElementById("password-numbers").style.color = "red";
+        }
+    }
+
+    // Contains at least one special character in the password
+    function containsSymbol(password) {
+        if (new RegExp('[^A-Za-z0-9]').test(password)) {
+            document.getElementById("password-special").style.color = "green";
+        } else {
+            document.getElementById("password-special").style.color = "red";
+        }
+    }
+
+    // No consecutive characters
+    function noSameLetterSequences(password) {
+        if (new RegExp('(.)\\1+').test(password)) {
+            document.getElementById("password-sequences").style.color = "red";
+        } else {
+            document.getElementById("password-sequences").style.color = "green";
+        }
+    }
+
+    // If the password is empty
+    function passwordEmpty(password) {
+        if (password === "") {
+            let passwordRequirements = document.querySelectorAll("#password-requirements *");
+            // Make all of the texts
+            for (let i = 0; i < passwordRequirements.length; i++) {
+                passwordRequirements[i].style.color = "black";
+            }
+            return true;
+        }
+        return false;
     }
 };
